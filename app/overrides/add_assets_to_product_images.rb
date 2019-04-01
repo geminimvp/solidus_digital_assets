a2b5a2d3d68e7b79919b1b051d40696fc88fc799
fastjames <fastjames@gmail.com>
2019-04-01 17:09:29 -0500

    fixup: disable assets shower on product images page
    I think this is what's breaking the page

--- app/overrides/add_assets_to_product_images.rb
+++ app/overrides/add_assets_to_product_images.rb
@@ -2,4 +2,5 @@ Deface::Override.new(virtual_path: 'spree/admin/images/index',
   name: 'add_assets_to_product_images',
   replace: "#images-table",
   partial: 'spree/admin/digital_assets/show_assets',
-  original: '8b0c556c850177cc2ff79ea5a18210014076d8b1')
+  original: '8b0c556c850177cc2ff79ea5a18210014076d8b1',
+  disabled: true)
